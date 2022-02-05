2838815e4c603d5bb24373fab28c474161a5ec26
withewolf-dev <itskashyap26@gmail.com>
2022-02-05 21:00:39 +0530

    test dep

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,35 @@
 import Head from 'next/head'
+import { SyntheticEvent, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+
+  const Login = async (e: SyntheticEvent) => {
+    e.preventDefault()
+    try {
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+        },
+        withCredentials: true,
+      }
+      const { data } = await axios.post(
+        `https://whatsappchat-server.herokuapp.com/api/user/login`,
+        {
+          email,
+          password,
+        },
+        config
+      )
+      console.log(data)
+    } catch (error) {
+      alert(error.response.data.message)
+    }
+    setemail('')
+    setpassword('')
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -8,75 +37,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
+      <input
+        className="bg-red-50"
+        value={email}
+        onChange={(e) => {
+          setemail(e.target.value)
+        }}
+      />
+      <input
+        className="bg-red-400"
+        value={password}
+        onChange={(e) => {
+          setpassword(e.target.value)
+        }}
+      />
+      <button onClick={Login}>login</button>
     </div>
   )
 }
